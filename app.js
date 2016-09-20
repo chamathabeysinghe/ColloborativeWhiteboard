6ec0de924bffdab47cda2862338e5a84284ae507
Chamath Abeysinghe <abeysinghechamath@gmail.com>
2016-09-20 16:47:10 +0530

    Simple work space using FabricJS

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var workspace=require('./routes/workspace')
 
 var app = express();
 
@@ -24,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/workspace',workspace);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
